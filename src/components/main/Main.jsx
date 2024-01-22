96bd92af20c26b5e1b54e74b31daf3d2252d64e0
leorega <leonardoregazzoni@gmail.com>
2024-01-22 17:23:31 -0300

    Agrego accordion a la sección Sobre Mí y actualizo CV

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,23 +5,33 @@ import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 const Main = () => {
   return (
-    <main id="home" className="flex flex-col items-center justify-center w-full min-h-screen bg-slate-500 shadow-2xl">
+    <main
+      id="home"
+      className="flex flex-col items-center justify-center w-full min-h-screen bg-slate-500 shadow-2xl"
+    >
       <div className=" md:w-4/5 flex flex-col items-center mb-32">
         <h1 className="font-extrabold text-blue-400 text-4xl md:text-6xl mb-4 drop-shadow-xl border-b-[6px] pb-4 border-green-500">
-        Leonardo Regazzoni
+          Leonardo Regazzoni
         </h1>
-        <h3 className="font-mono font-bold text-xl md:text-2xl">Desarrollador Web FullStack</h3>
+        <h3 className="font-mono font-bold text-xl md:text-2xl">
+          Desarrollador Web FullStack
+        </h3>
         <div className="mt-4 flex justify-center items-center">
           <MdOutlineKeyboardDoubleArrowLeft className="text-2xl text-green-500" />
-          <a href="./CV Leonardo Regazzoni.pdf" title="Descargar" download className="flex flex-col items-center text-blue-400 font-bold text-xl mx-4 hover:text-green-500">
-            <MdCloudDownload className="mx-2 animate-bounce"/> CV
+          <a
+            href="./Leonardo Regazzoni CV.pdf"
+            title="Descargar"
+            download
+            className="flex flex-col items-center text-blue-400 font-bold text-xl mx-4 hover:text-green-500"
+          >
+            <MdCloudDownload className="mx-2 animate-bounce" /> CV
           </a>
           <MdOutlineKeyboardDoubleArrowRight className="text-2xl text-green-500" />
         </div>
       </div>
-      <Slider/>
+      <Slider />
     </main>
-  )
-}
+  );
+};
 
 export default Main;
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,30 +3,56 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 
 const NavBar = () => {
-
   const [state, setState] = useState("hidden");
 
   return (
-    <header className='bg-slate-600 lg:bg-opacity-70 w-full h-20 fixed top-0 z-10 flex items-center mb-8 shadow-lg'>
-      <div className='flex w-4/5 mx-auto justify-between items-center h-full'>
-        <div className='flex font-black text-3xl drop-shadow-md cursor-pointer'>
+    <header className="bg-slate-600 lg:bg-opacity-70 w-full h-20 fixed top-0 z-10 flex items-center mb-8 shadow-lg">
+      <div className="flex w-4/5 mx-auto justify-between items-center h-full">
+        <div className="flex font-black text-3xl drop-shadow-md cursor-pointer">
           <Link to="home" smooth={true} duration={500} className="flex">
-            <h2 translate="no" className='text-blue-400'>Leo</h2><h2 translate="no" className='text-green-500'>Rega!</h2>
+            <h2 translate="no" className="text-blue-400">
+              Leo
+            </h2>
+            <h2 translate="no" className="text-green-500">
+              Rega!
+            </h2>
           </Link>
         </div>
         <div className="lg:hidden">
-          <button onClick={()=>(setState(state==='hidden' ? 'block' : 'hidden'))}>
-            <GiHamburgerMenu className="text-2xl text-blue-400 active:text-green-500"/>
+          <button
+            onClick={() => setState(state === "hidden" ? "block" : "hidden")}
+          >
+            <GiHamburgerMenu className="text-2xl text-blue-400 active:text-green-500" />
           </button>
         </div>
-        <nav className={`${state} text-blue-400 bg-slate-600 w-1/2 flex flex-col fixed top-16 right-0 h-40 items-center justify-around rounded-bl-xl lg:flex lg:flex-row lg:static lg:bg-opacity-10 lg:h-full lg:rounded-none lg:w-1/4`}>
-          <a onClick={()=>(setState(state==='hidden' ? 'block' : 'hidden'))} className='pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400' href="#about">Sobre mí</a>
-          <a onClick={()=>(setState(state==='hidden' ? 'block' : 'hidden'))} className='pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400' href="#projects">Proyectos</a>
-          <a onClick={()=>(setState(state==='hidden' ? 'block' : 'hidden'))} className='pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400' href="#contact">Contacto</a>
+        <nav
+          className={`${state} text-blue-400 bg-slate-600 w-1/2 flex flex-col fixed top-16 right-0 h-40 items-center justify-around rounded-bl-xl lg:flex lg:flex-row lg:static lg:bg-opacity-10 lg:h-full lg:rounded-none lg:w-1/4`}
+        >
+          <a
+            onClick={() => setState(state === "hidden" ? "block" : "hidden")}
+            className="pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400"
+            href="#about"
+          >
+            Sobre mí
+          </a>
+          <a
+            onClick={() => setState(state === "hidden" ? "block" : "hidden")}
+            className="pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400"
+            href="#projects"
+          >
+            Proyectos
+          </a>
+          <a
+            onClick={() => setState(state === "hidden" ? "block" : "hidden")}
+            className="pt-2 lg:hover:border-t-2 hover:font-bold duration-150 border-blue-400"
+            href="#contact"
+          >
+            Contacto
+          </a>
         </nav>
       </div>
     </header>
   );
-}
+};
 
 export default NavBar;
