c74e43a64d6e6ed75921325d4546634e0babfca2
leorega <leonardoregazzoni@gmail.com>
2024-02-06 10:44:31 -0300

    Cambios mínimos en textos de About y Form

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,94 +1,121 @@
 import { Accordion } from "keep-react";
 
 const About = () => {
-  return (
-    <section
-      id="about"
-      className="w-full min-h-screen pt-20 bg-green-500 flex flex-col items-center justify-center"
-    >
-      <div translate="no" className="md:w-4/5 md:border-l-4 border-black p-4">
-        <Accordion openFirstPanel={true} className="shadow-3xl">
-          <Accordion.Panel>
-            <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
-              <Accordion.Title>Sobre mí</Accordion.Title>
-            </Accordion.Container>
-            <Accordion.Content className="border-b-2 border-slate-600">
-              <p className="font-semibold">
-                Trabajé en una planta envasadora de agua mineral, donde me
-                desarrollé haciendo tareas muy variadas, desde la operación de
-                máquinas de última tecnología, liderar un equipo de producción y
-                capacitar operadores. Hasta que decidí cambiar de rumbo mi vida
-                ya que me encontraba estancado, sin una motivación real,
-                auténtica; entonces decidí meterme de lleno en algo que siempre
-                me gustó: la <strong>programación</strong>.
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
-              <Accordion.Title>Stack Tecnológico</Accordion.Title>
-            </Accordion.Container>
-            <Accordion.Content className="border-b-2 border-slate-600">
-              <p className="font-semibold">
-                Comencé tomando un curso de{" "}
-                <strong>desarrollo web en Next-U</strong>, que se basaba en{" "}
-                <strong>Python</strong>. Después hice el primer año de la{" "}
-                <strong>tecnicatura superior en programación en el IRSO</strong>
-                , donde vi las bases de <b>C</b>, pero tuve que dejarla por la
-                pandemia de COVID.
-              </p>
-              <p className="font-semibold">
-                <i className="text-red-800 text-lg">
-                  No iba a darme por vencido!
-                </i>
-                , así que me puse a estudiar nuevamente y entonces me metí en la
-                carrera de
-                <strong> Web Developer FullStack de Henry</strong> y en la{" "}
-                <strong>
-                  Diplomatura en Desarrollo Web Full Stack de la Universidad
-                  Tecnológica Nacional
-                </strong>
-                , de las cuales estoy graduado.
-              </p>
-              <p className="font-semibold">
-                En estas carreras aprendí a desarrollar en{" "}
-                <strong>JavaScript</strong> y <strong>PHP</strong>{" "}
-                respectivamente. Aprendí a trabajar con distintas tecnologías
-                como:{" "}
-                <strong>
-                  React, Redux, HTML, CSS, sql, MySql, Postgres, Sequelize,
-                </strong>{" "}
-                entre las mas destacadas. También aprendí por mi cuenta
-                <strong> Tailwind, Mongo, Laravel, Next, Bootstrap</strong> y
-                sigo incorporando conocimientos. Manejo muy bien control de
-                versiones con <strong>Git, GitHub, GitLab</strong>
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
-              <Accordion.Title>Soft Skills</Accordion.Title>
-            </Accordion.Container>
-            <Accordion.Content className="border-b-2 border-slate-600">
-              <p className="font-semibold">
-                En el transcurso de la tecnicatura y sobre todo de las
-                diplomaturas, aprendí a trabajar con metodologías ágiles de
-                trabajo en equipo como <strong>SCRUM</strong>. <br />{" "}
-                Organización de trabajo con tableros tipo Trello. <br />
-                También tengo experiencia trabajando en equipo con control de
-                versiones en GitHub. <br />
-                Tengo un nivel de inglés B2 y sigo estudiando. <br />
-                En lo relacionado a la adaptabilidad me destaco en la resolución
-                de problemas y tolerancia al cambio. <br />
-                Mi cultura de trabajo ideal es Commander, la cual valora el
-                orden, la estructura y la eficiencia.
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-        </Accordion>
-      </div>
-    </section>
-  );
+    return (
+        <section
+            id="about"
+            className="w-full min-h-screen pt-20 bg-green-500 flex flex-col items-center justify-center"
+        >
+            <div
+                translate="no"
+                className="md:w-4/5 md:border-l-4 border-black p-4"
+            >
+                <Accordion openFirstPanel={true} className="shadow-3xl">
+                    <Accordion.Panel>
+                        <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
+                            <Accordion.Title>Sobre mí</Accordion.Title>
+                        </Accordion.Container>
+                        <Accordion.Content className="border-b-2 border-slate-600">
+                            <p className="font-semibold">
+                                Trabajé en una planta envasadora de agua
+                                mineral, donde me desarrollé haciendo tareas muy
+                                variadas, desde la operación de máquinas de
+                                última tecnología, liderar un equipo de
+                                producción y capacitar operadores.{" "}
+                                <strong>
+                                    {" "}
+                                    Hasta que decidí cambiar de rumbo mi vida
+                                </strong>
+                                ya que me encontraba estancado, sin una
+                                motivación real, auténtica; entonces decidí
+                                meterme de lleno en algo que siempre me gustó:
+                                la <strong>programación</strong>.
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                    <Accordion.Panel>
+                        <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
+                            <Accordion.Title>Stack Tecnológico</Accordion.Title>
+                        </Accordion.Container>
+                        <Accordion.Content className="border-b-2 border-slate-600">
+                            <p className="font-semibold">
+                                Comencé tomando un curso de{" "}
+                                <strong>desarrollo web en Next-U</strong>, que
+                                se basaba en <strong>Python</strong>. Después
+                                hice el primer año de la{" "}
+                                <strong>
+                                    tecnicatura superior en programación en el
+                                    IRSO
+                                </strong>
+                                , donde vi las bases de <b>C</b>, pero tuve que
+                                dejarla por la pandemia de COVID.
+                            </p>
+                            <p className="font-semibold">
+                                <i className="text-red-800 text-lg">
+                                    No iba a darme por vencido!
+                                </i>
+                                , así que me puse a estudiar nuevamente y
+                                entonces me metí en la carrera de
+                                <strong>
+                                    {" "}
+                                    Web Developer FullStack de Henry
+                                </strong>{" "}
+                                y en la{" "}
+                                <strong>
+                                    Diplomatura en Desarrollo Web Full Stack de
+                                    la Universidad Tecnológica Nacional
+                                </strong>
+                                , de las cuales estoy graduado.
+                            </p>
+                            <p className="font-semibold">
+                                En estas carreras aprendí a desarrollar en{" "}
+                                <strong>JavaScript</strong> y{" "}
+                                <strong>PHP</strong> respectivamente. Aprendí a
+                                trabajar con distintas tecnologías como:{" "}
+                                <strong>
+                                    React, Redux, HTML, CSS, sql, MySql,
+                                    Postgres, Sequelize,
+                                </strong>{" "}
+                                entre las mas destacadas. También aprendí por mi
+                                cuenta
+                                <strong>
+                                    {" "}
+                                    Tailwind, Mongo, Laravel, Next, Bootstrap
+                                </strong>{" "}
+                                y sigo incorporando conocimientos. Manejo muy
+                                bien control de versiones con{" "}
+                                <strong>Git, GitHub, GitLab</strong>
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                    <Accordion.Panel>
+                        <Accordion.Container className="bg-green-200 border-b-2 border-slate-400">
+                            <Accordion.Title>Soft Skills</Accordion.Title>
+                        </Accordion.Container>
+                        <Accordion.Content className="border-b-2 border-slate-600">
+                            <p className="font-semibold">
+                                En el transcurso de la tecnicatura y sobre todo
+                                de las diplomaturas, aprendí a trabajar con
+                                metodologías ágiles de trabajo en equipo como{" "}
+                                <strong>SCRUM</strong>. <br /> Organización de
+                                trabajo con tableros tipo Trello. <br />
+                                También tengo experiencia trabajando en equipo
+                                con control de versiones en GitHub. <br />
+                                Tengo un nivel de inglés B2 y sigo estudiando.{" "}
+                                <br />
+                                En lo relacionado a la adaptabilidad me destaco
+                                en la resolución de problemas y tolerancia al
+                                cambio. <br />
+                                Mi cultura de trabajo ideal es Commander, la
+                                cual valora el orden, la estructura y la
+                                eficiencia.
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                </Accordion>
+            </div>
+        </section>
+    );
 };
 
 export default About;
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,52 +1,97 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import Swal from 'sweetalert2';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Form = () => {
+    const form = useRef();
 
-  const form = useRef();
+    const showAlert = () => {
+        Swal.fire({
+            toast: true,
+            icon: "success",
+            title: "Gracias por tu mensaje",
+            text: "De ser necesario, te contestaré lo antes posible",
+            showConfirmButton: true,
+            position: "center",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById("form").reset();
+            }
+        });
+    };
 
-  const showAlert = () => {
-    Swal.fire({
-      toast: true,
-      icon: "success",
-      title: "Gracias por tu mensaje",
-      text: "De ser necesario, te contestaré lo antes posible",
-      showConfirmButton: true,
-      position: "center"
-    }).then((result) => {
-      if(result.isConfirmed) {
-        document.getElementById("form").reset();
-      }
-    });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+        emailjs
+            .sendForm(
+                "service_02fwqi8",
+                "template_l1rimze",
+                form.current,
+                "GXkdmm6WR3rIj8-TN"
+            )
+            .then(
+                (result) => {
+                    showAlert();
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
 
-    emailjs.sendForm('service_02fwqi8', 'template_l1rimze', form.current, 'GXkdmm6WR3rIj8-TN')
-      .then((result) => {
-        showAlert();
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-  };
-
-  return (
-    <section id='contact' className='w-full h-screen flex flex-col pt-20 items-center bg-green-500'>
-        <h3 className='text-3xl font-bold drop-shadow-[1px_1px_5px_rgb(96,165,250)] mt-2 mb-2'>Formulario de contacto</h3>
-        <form id='form' ref={form} onSubmit={handleSubmit} method="POST" className='w-full h-4/5 md:w-4/5 lg:w-3/5'>
-          <fieldset className='border-2 border-black flex flex-col mx-2 h-full items-center justify-around shadow-2xl'>
-            <legend className='text-xl m-2 p-2'>Déjame tu mensaje</legend>
-            <input className='w-3/4 my-2 rounded-sm border-2 border-black p-2' type="text" name='name' placeholder='Nombre' required/>
-            <input className='w-3/4 my-2 rounded-sm border-2 border-black p-2' type="email" name='email' placeholder='Email' required/>
-            <textarea className='w-3/4 my-2 rounded-sm border-2 border-black p-2' name="message" cols="30" rows="10" placeholder='Escribe tu mensaje acá' required></textarea>
-            <button type='submit' className='bg-blue-400 w-1/4 h-10 rounded-md my-2 border-2 border-black hover:bg-slate-700 hover:text-blue-200 hover:font-bold duration-500'>Enviar</button>
-          </fieldset>
-        </form>
-    </section>
-  );
-}
+    return (
+        <section
+            id="contact"
+            className="w-full h-screen flex flex-col pt-20 items-center bg-green-500"
+        >
+            <h3 className="text-3xl font-bold drop-shadow-[1px_1px_5px_rgb(96,165,250)] mt-2 mb-2">
+                Conversemos
+            </h3>
+            <form
+                id="form"
+                ref={form}
+                onSubmit={handleSubmit}
+                method="POST"
+                className="w-full h-4/5 md:w-4/5 lg:w-3/5"
+            >
+                <fieldset className="border-2 border-black flex flex-col mx-2 h-full items-center justify-around shadow-2xl">
+                    <legend className="text-xl m-2 p-2">
+                        Emprendamos juntos nuevos proyectos
+                    </legend>
+                    <input
+                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        type="text"
+                        name="name"
+                        placeholder="Nombre"
+                        required
+                    />
+                    <input
+                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <textarea
+                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        name="message"
+                        cols="30"
+                        rows="10"
+                        placeholder="Escribe tu mensaje acá"
+                        required
+                    ></textarea>
+                    <button
+                        type="submit"
+                        className="bg-blue-400 w-1/4 h-10 rounded-md my-2 border-2 border-black hover:bg-slate-700 hover:text-blue-200 hover:font-bold duration-500"
+                    >
+                        Enviar
+                    </button>
+                </fieldset>
+            </form>
+        </section>
+    );
+};
 
 export default Form;
