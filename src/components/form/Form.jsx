0f35d1c5c17940c3f25c63d73a7cac05bd86dd0a
leorega <leonardoregazzoni@gmail.com>
2025-02-11 17:41:40 -0300

    Modifico aspecto formulario

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -46,7 +46,7 @@ const Form = () => {
             id="contact"
             className="w-full h-screen flex flex-col pt-20 items-center bg-green-500"
         >
-            <h3 className="text-3xl font-bold drop-shadow-[1px_1px_5px_rgb(96,165,250)] mt-2 mb-2">
+            <h3 className="text-3xl text-gray-300 font-bold drop-shadow-[1px_1px_5px_rgb(96,165,250)] mt-2 mb-2">
                 Conversemos
             </h3>
             <form
@@ -56,26 +56,26 @@ const Form = () => {
                 method="POST"
                 className="w-full h-4/5 md:w-4/5 lg:w-3/5"
             >
-                <fieldset className="border-2 border-black flex flex-col mx-2 h-full items-center justify-around shadow-2xl">
-                    <legend className="text-xl m-2 p-2">
+                <fieldset className="border-2 border-gray-300 rounded-xl  flex flex-col mx-2 h-full items-center justify-around shadow-2xl">
+                    <legend className="text-xl text-gray-300 m-2 p-2 drop-shadow-[1px_1px_5px_rgb(96,165,250)]">
                         Emprendamos juntos nuevos proyectos
                     </legend>
                     <input
-                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        className="w-3/4 my-2 rounded-xl border-2 border-gray-300 p-2"
                         type="text"
                         name="name"
                         placeholder="Nombre"
                         required
                     />
                     <input
-                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        className="w-3/4 my-2 rounded-xl border-2 border-gray-300 p-2"
                         type="email"
                         name="email"
                         placeholder="Email"
                         required
                     />
                     <textarea
-                        className="w-3/4 my-2 rounded-sm border-2 border-black p-2"
+                        className="w-3/4 my-2 rounded-xl border-2 border-gray-300 p-2"
                         name="message"
                         cols="30"
                         rows="10"
